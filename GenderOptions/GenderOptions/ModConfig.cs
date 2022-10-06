e4c199a4a1192a73e1b3847b7c12646b43f258be
RaptorReed <raptorrreed@gmail.com>
2022-10-06 03:41:23 -0400

    Adds initial content.

---
+++ GenderOptions/GenderOptions/ModConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenderOptions
+{
+    internal class ModConfig
+    {
+        public bool ExampleBoolean { get; set; } = true;
+        public int ExampleNumber { get; set; } = 5;
+        public string ExampleString { get; set; } = "This is my genderoptions test string";
+    }
+}
