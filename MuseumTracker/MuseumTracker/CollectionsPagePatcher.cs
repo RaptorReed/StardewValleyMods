e8486311424793ac92d08ed658be2dd57dd967d4
RaptorReed <raptorrreed@gmail.com>
2022-10-08 01:35:09 -0400

    Gets the harmony patch to successfully change color overlay.

--- MuseumTracker/MuseumTracker/CollectionsPagePatcher.cs
+++ MuseumTracker/MuseumTracker/CollectionsPagePatcher.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
@@ -25,21 +26,18 @@ namespace MuseumTracker
 
         public static void Postfix_Draw(CollectionsPage __instance, SpriteBatch b)
         {
-            /*CollectionsPage colPage = __instance;
-
+            CollectionsPage collPage = __instance;
+            b.End();
             b.Begin((SpriteSortMode)4, BlendState.AlphaBlend, SamplerState.PointClamp, (DepthStencilState)null, (RasterizerState)null, (Effect)null, (Matrix?)null);
-            foreach (ClickableTextureComponent item in colPage.collections[colPage.currentTab][colPage.currentPage])
+            if(collPage.currentTab == 2)
             {
-                bool flag = Convert.ToBoolean(item.name.Split(' ')[1]);
-                bool flag2 = colPage.currentTab == 4 && Convert.ToBoolean(item.name.Split(' ')[2]);
-                item.draw(b, flag2 ? (Color.DimGray * 0.4f) : (flag ? Color.White : (Color.Black * 0.2f)), 0.86f);
-                if (colPage.currentTab == 5 && flag)
+                foreach (ClickableTextureComponent item in collPage.collections[2][collPage.currentPage])
                 {
-                    int num = new Random(Convert.ToInt32(item.name.Split(' ')[0])).Next(12);
-                    b.Draw(Game1.mouseCursors, new Vector2((float)(item.bounds.X + 16 + 16), (float)(item.bounds.Y + 20 + 16)), (Rectangle?)new Rectangle(256 + num % 6 * 64 / 2, 128 + num / 6 * 64 / 2, 32, 32), Color.get_White(), 0f, new Vector2(16f, 16f), item.scale, (SpriteEffects)0, 0.88f);
+                    bool flag = Convert.ToBoolean(item.name.Split(' ')[1]);
+                    bool flag2 = collPage.currentTab == 4 && Convert.ToBoolean(item.name.Split(' ')[2]);
+                    item.draw(b, flag2 ? (Color.DimGray * 0.4f) : (flag ? Color.White : (Color.HotPink * 0.2f)), 0.86f);
                 }
             }
-            b.End();*/
         }
     }
 }
